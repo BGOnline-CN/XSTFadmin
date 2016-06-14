ad32a8344ef38459282b8e59c091e5be0c59a1eb
BGOnline <676114734@qq.com>
2016-06-14 10:09:38 +0800

    同步成功

--- app/js/app.js
+++ app/js/app.js
@@ -6,10 +6,8 @@
 
 if (typeof $ === 'undefined') { throw new Error('This application\'s JavaScript requires jQuery'); }
 
-//测试一下
 
 // 全局
-
 var url = 'http://xiaoshutong.thinktorch.cn/frontend/web'; // API
 var rootUrl = 'http://xiaoshutong.thinktorch.cn';
 var noF5speed = 5; // 无刷新获取数据的速度 秒
